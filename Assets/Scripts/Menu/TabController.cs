a8c78dbe51dcd1881f5001fc5b4653988119253a
Gianniiii <gianni.lodetti@hotmail.com>
2022-10-31 11:04:15 +0100

    spawned tab now has default content and is called by its button onClick

--- Assets/Scripts/Menu/TabController.cs
+++ Assets/Scripts/Menu/TabController.cs
@@ -3,34 +3,56 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using System.Text;
+using UnityEngine.UI;
 
 public class TabController : MonoBehaviour
 {
-    [SerializeField] private GameObject[] tabs;
-    public GameObject spawnButtonPrefab;
+    [SerializeField] private List<GameObject> tabs = new List<GameObject>();
+    //For spawning of tab button
+    public Button spawnButtonPrefab;
     public GameObject parentOfSpawn;
-    private GameObject spawnedTabButton;
-    private TextMeshProUGUI spawnedText;
-    private int choiceId = 0;
+    private Button spawnedTabButton;
 
-    public void onTabSwitch(GameObject tab) {
-        tab.SetActive(true);
+    //For spawning for tab body
+    public GameObject parentOfBodySpawn; //to know where to place the tab in the hierarchy
+    public GameObject spawnTabBodyPrefab;
+    private GameObject spawnedTabBody; //new obj for spawned body
+    //--
+    private int choiceId = 0; //used to give spawnedTab a name
 
-        for(int i = 0; i < tabs.Length; i++) {
-            if(tabs[i]!= tab){
+    //Called by the pressed button, with as tabBody the body associated with the button
+    public void onTabSwitch(GameObject tabBody) {
+        tabBody.SetActive(true);  
+
+        for(int i = 0; i < tabs.Count; i++) {
+            if(tabs[i]!= tabBody){
                 tabs[i].SetActive(false);
             }
         }
     }
 
+    //could return the spawned tabs so we can modify them later if needed, for now just let them be.
     public void spawnTab(string buttonText, string tabContent) {
         //Spawn a button for the tab
         spawnedTabButton = GameObject.Instantiate(spawnButtonPrefab);
         spawnedTabButton.GetComponentInChildren<TextMeshProUGUI>().text = buttonText;
-        spawnedTabButton.name = "Tab_"+ choiceId.ToString();
+        spawnedTabButton.name = "spawnedTabButton_"+ choiceId.ToString();
         spawnedTabButton.transform.SetParent(parentOfSpawn.transform, false);
 
-        //Spawn new tab with contents
+        //Spawn new tab with 
+        spawnedTabBody = GameObject.Instantiate(spawnTabBodyPrefab);
+        spawnedTabBody.name = "spawnedTabBody_"+ choiceId.ToString();
+        spawnedTabBody.transform.SetParent(parentOfBodySpawn.transform, false);
+
+        //Set content of new spawnedTab
+        //spawnedTabBody.GetComponentInChildren<TextMeshProUGUI>().text = tabContent; //oh yeah.. obvvv
+
+        //Modify the tab body that the spawned Button will call
+        //override previous onTabSwitch call from onClick method
+        spawnedTabButton.onClick.AddListener(delegate { onTabSwitch(spawnedTabBody); });
+
+        //Add new spawned tab to tabs array
+        tabs.Add(spawnedTabBody);
         
     }
 }
